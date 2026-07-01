454fe464fdc1db4ffb542fff8032bd3dd6c67ce5
Alkumist <ponyyouman@gmail.com>
2026-07-01 00:02:52 -0600

    using in mod graphics, and fixed lack of recipe icon changing

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,65 +1,33 @@
-local meld = require("meld")
+data.raw.item["automation-science-pack"].icon = "__cb-science-Alk__/graphics/icons/automation.png"
+data.raw.technology["automation-science-pack"].icon = "__cb-science-Alk__/graphics/technology/automation.png"
 
-local science_packs = {
-	["automation-science-pack"] = {
-		{ model = "cone_slim", variant = "liquid_red" },
-		{ variant = "liquid_red" },
-	},
-	["logistic-science-pack"] = {
-		{ model = "cube", variant = "liquid_green" },
-		{ variant = "liquid_green" },
-	},
-	["military-science-pack"] = {
-		{ model = "sphere_spiked", variant = "liquid_black" },
-		{ variant = "liquid_black" },
-	},
-	["chemical-science-pack"] = {
-		{ model = "sphere_tiny", variant = "liquid_cyan" },
-		{ variant = "liquid_cyan" },
-	},
-	["production-science-pack"] = {
-		{ model = "sphere_double", variant = "liquid_purple" },
-		{ variant = "liquid_purple" },
-	},
-	["utility-science-pack"] = {
-		{ model = "triangle", variant = "liquid_yellow" },
-		{ variant = "liquid_yellow" },
-	},
-	["space-science-pack"] = {
-		{ model = "cone_inverted", variant = "liquid_white" },
-		{ variant = "liquid_white" },
-	},
-	["metallurgic-science-pack"] = {
-		{ model = "hexagon", variant = "liquid_orange" },
-		{ variant = "liquid_orange" },
-	},
-	["electromagnetic-science-pack"] = {
-		{ model = "hourglass", variant = "liquid_pink" },
-		{ variant = "liquid_pink" },
-	},
-	["agricultural-science-pack"] = {
-		{ model = "tube_one", variant = "liquid_lime" },
-		{ variant = "liquid_lime" },
-	},
-	["cryogenic-science-pack"] = {
-		{ model = "cone_normal", variant = "frozen_blue" },
-		{ variant = "frozen_blue" },
-		{ variant = "liquid_blue" },
-	}
-}
+data.raw.item["logistic-science-pack"].icon = "__cb-science-Alk__/graphics/icons/logistic.png"
+data.raw.technology["logistic-science-pack"].icon = "__cb-science-Alk__/graphics/technology/logistic.png"
 
-for name, requests in pairs(science_packs) do
-	local variant = quality_glassware.request_flask(name, requests)
-	if data.raw["technology"][name] then
-		meld(
-			data.raw["technology"][name],
-			quality_glassware.technology_graphics_for(variant, meld)
-		)
-	end
-	if data.raw["tool"][name] then
-		meld(
-			data.raw["tool"][name],
-			quality_glassware.item_graphics_for(variant, meld)
-		)
-	end
-end
+data.raw.item["military-science-pack"].icon = "__cb-science-Alk__/graphics/icons/military.png"
+data.raw.technology["military-science-pack"].icon = "__cb-science-Alk__/graphics/technology/military.png"
+
+data.raw.item["chemical-science-pack"].icon = "__cb-science-Alk__/graphics/icons/chemical.png"
+data.raw.technology["chemical-science-pack"].icon = "__cb-science-Alk__/graphics/technology/chemical.png"
+
+data.raw.item["production-science-pack"].icon = "__cb-science-Alk__/graphics/icons/production.png"
+data.raw.technology["production-science-pack"].icon = "__cb-science-Alk__/graphics/technology/production.png"
+
+data.raw.item["utility-science-pack"].icon = "__cb-science-Alk__/graphics/icons/utility.png"
+data.raw.technology["utility-science-pack"].icon = "__cb-science-Alk__/graphics/technology/utility.png"
+
+data.raw.item["space-science-pack"].icon = "__cb-science-Alk__/graphics/icons/space.png"
+data.raw.technology["space-science-pack"].icon = "__cb-science-Alk__/graphics/technology/space.png"
+data.raw.recipe["space-science-pack"].icon = "__cb-science-Alk__/graphics/icons/space.png"
+
+data.raw.item["metallurgic-science-pack"].icon = "__cb-science-Alk__/graphics/icons/metallurgic.png"
+data.raw.technology["metallurgic-science-pack"].icon = "__cb-science-Alk__/graphics/technology/metallurgic.png"
+
+data.raw.item["electromagnetic-science-pack"].icon = "__cb-science-Alk__/graphics/icons/electromagnetic.png"
+data.raw.technology["electromagnetic-science-pack"].icon = "__cb-science-Alk__/graphics/technology/electromagnetic.png"
+
+data.raw.item["agricultural-science-pack"].icon = "__cb-science-Alk__/graphics/icons/agricultural.png"
+data.raw.technology["agricultural-science-pack"].icon = "__cb-science-Alk__/graphics/technology/agricultural.png"
+
+data.raw.item["cryogenic-science-pack"].icon = "__cb-science-Alk__/graphics/icons/cryogenic.png"
+data.raw.technology["cryogenic-science-pack"].icon = "__cb-science-Alk__/graphics/technology/cryogenic.png"
